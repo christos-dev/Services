4bbc65f5bcebadb55fbed04de0f092ee437243c7
Christos Zacharakis <zaxrii@gmail.com>
2021-10-15 17:19:36 +0300

    current time added

--- app/src/main/java/com/example/alarmmanagerkotlin/AlarmRandomNumber.kt
+++ app/src/main/java/com/example/alarmmanagerkotlin/AlarmRandomNumber.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.util.Log
 import java.util.*
 import android.os.Handler
-import androidx.core.app.NotificationCompat
 
 class AlarmRandomNumber() : BroadcastReceiver() {
 
@@ -15,13 +14,12 @@ class AlarmRandomNumber() : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         showRandomNumber()
-//        var builder = NotificationCompat.Builder(context!!, "foxandroid")
     }
 
     private fun showRandomNumber() {
         val rand = Random()
         val number = rand.nextInt(100)
-        Log.d("RandomNumber", "Random number : $number" )
+        Log.d("RandomNumber", "Random number : $number Time : ${Date()}")
     }
 
 }
